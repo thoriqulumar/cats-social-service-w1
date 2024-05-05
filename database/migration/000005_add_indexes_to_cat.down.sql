4ad83ff62ee5e47481cac323473c3b07b0ffbce6
Zulfikar Fajri <fajrizulfikar85@gmail.com>
2024-05-05 16:04:34 +0700

    fix: migration-down script

--- database/migration/000005_add_indexes_to_cat.down.sql
+++ database/migration/000005_add_indexes_to_cat.down.sql
@@ -1,2 +1,6 @@
--- Drop the table
-DROP TABLE IF EXISTS "cat";
+DROP INDEX idx_cat_race;
+DROP INDEX idx_cat_sex;
+DROP INDEX idx_cat_age;
+DROP INDEX idx_cat_owner;
+DROP INDEX idx_cat_text_search;
+DROP INDEX idx_cat_matched;
